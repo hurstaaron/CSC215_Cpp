608c79a9b0cc998278e91250b2de609b89b74b7b
Aaron <hurst.aaron@outlook.com>
2026-02-16 22:21:58 -0700

    Update random numbers project and ignore VS artifacts

--- Random_Numbers_and_Arrays_in_Cpp/Random_Numbers_and_Arrays_in_Cpp.cpp
+++ Random_Numbers_and_Arrays_in_Cpp/Random_Numbers_and_Arrays_in_Cpp.cpp
@@ -1,20 +1,63 @@
-// Random_Numbers_and_Arrays_in_Cpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
+// Include the preprocessing libraries needed for the code to work
 #include <iostream>
+#include <string>
+#include <array>
+#include <cstdlib>
 
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+
+int main(){
+	// Add srand() to ensure a new reandom number is generated everytime rand() is executed
+	srand(time(0));
+
+	// Declare an int array of size 20 and initialize all elements to 0
+	int twenty_randomNumbers[20] { };
+	
+	// For 20 interations generate a new random number and assign it to a index in the array
+	for (int i{ 0 }; i < 20; i++) {
+		int randomNum = rand() % 101; // Declare int var to hold the random number
+		twenty_randomNumbers[i] = randomNum; // Assign the random number to the ith index in the array
+		cout << "For iteration " << i + 1 << ", the number is " << twenty_randomNumbers[i] << ". \n"; // Display the ith index in the array
+	}
+	cout << endl;
+
+	
+	//cout << "\n\n======= For validation purposes ======= \n";
+	//cout << "The size of the array is " << sizeof(twenty_randomNumbers) << " bytes.\n";
+	//cout << "This is because the size of an int is 4 bytes.\n" 
+	//	<< "The array has 20 int variables; and 4 times 20 is " << sizeof(twenty_randomNumbers) << endl << endl;
+	
+
+	// For 20 iterations, search the array to find the smallest number
+	int minValue = twenty_randomNumbers[0]; // To find minValue, start at the first element of the array
+	for (int i{ 0 }; i < 20; i++) {
+		if (twenty_randomNumbers[i] < minValue) {
+			minValue = twenty_randomNumbers[i]; // assign minValue with the number at the index if it is smaller than the ith number
+		}
+	}
+	cout << minValue << " is the smallest number in the array.\n"; // Display the smallest number
+	
+
+	// For 20 iterations, search the array to find the largest number
+	int maxValue = twenty_randomNumbers[0]; // To find maxValue, start at the first element of the array
+	for (int i{ 0 }; i < 20; i++) {
+		if (twenty_randomNumbers[i] > maxValue) {
+			maxValue = twenty_randomNumbers[i]; // assign maxValue with the number at the index if it is larger than the ith number
+		}
+	}
+	cout << maxValue << " is the largest number in the array.\n";
+
+
+	// For 20 iterations, search the array to find the largest number
+	int sumOfValues = 0; // To find sumOfValues, start at the first element of the array
+	for (int i{ 0 }; i < 20; i++) {
+		sumOfValues += twenty_randomNumbers[i]; // Add each number in the array to the next
+	}
+	cout << sumOfValues << " is the sum of the numbers in the array.\n";
+
+
+	int averageOfValues = sumOfValues / 20; // Evaluate to find the average of the numbers in the array
+	cout << averageOfValues << " is the average of the numbers in the array.\n";
+
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
