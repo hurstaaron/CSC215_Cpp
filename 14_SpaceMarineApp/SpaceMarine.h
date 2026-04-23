17b2584c183d03764b829b23b617a4208bdf38ec
Aaron Hurst <hurst.aaron1@gmail.com>
2026-04-22 22:18:06 -0700

    Better desing: using '=' instead of '#' for flagging code blocks

--- 14_SpaceMarineApp/SpaceMarine.h
+++ 14_SpaceMarineApp/SpaceMarine.h
@@ -9,11 +9,11 @@
 #include <string>       // We want to use the string data type
 using namespace std;    // Standard namespace makes it so we don't have to write std:: all the time
 
-// ############### DECLARE CLASS: SpaceMarine ###############
+// =============== DECLARE CLASS: SpaceMarine ===============
 // A Space Marine object exists and here are the attributes 
 // (what it knows) and the methods (what it can do)
 class SpaceMarine{
-// ############### ENCAPSULATION!!! - DECLARE PRIVATE ###############
+// =============== ENCAPSULATION!!! - DECLARE PRIVATE ===============
 // The "private" access modifier means only THIS calss can use these variables
 // This is an example of ENCAPSULATION in OOP, one of the most important and 
 // powerful aspects of OOP. Encapsulation allows us to hide our data inside
@@ -27,13 +27,13 @@ private:
 // Declare PUBLIC functions
 // The "public" access modifier means these functions can be used anywhere, even outside the class
 public:
-    // ############### DECLARE CONSTRUCTOR ###############
+    // =============== DECLARE CONSTRUCTOR ===============
     // Constructors are special function that runs automatically as soon as the Space Marine
     // object is created.  Sets up the starting values of the Space Marine object.
     // Constructors NEVER have return types.
     SpaceMarine(string marineName, string marineRole, int power, bool veteran);
 
-    // ############### DECLARE GETTERS ###############  
+    // =============== DECLARE GETTERS ===============  
     // Getter methods return the private variables and allow the outside world (public) 
     // to read the values through these 'getter methods".
     // Use "const" at the end so they will NOT change the objects data.
@@ -43,7 +43,7 @@ public:
     int getCombatPower() const;
     bool getIsVeteran() const;
     
-    // ############### DECLARE SETTERS ###############  
+    // =============== DECLARE SETTERS ===============  
     // Setter methods are functions that will allow CHANGES to the private variables.
     // The outside world (public) will have the abilitiy to WRITE new data through them.
     // Notice we just add "set" infront of our variable name. 
@@ -52,7 +52,7 @@ public:
     void setCombatPower(int newPower);
     void setIsVeteran(bool veteran);
 
-    // ############### DECLARE DISPLAY ############### 
+    // =============== DECLARE DISPLAY =============== 
     // This function prints out the Marine's info.
     // It is delcared at the end so every Space Marine object created 
     // will know how to display itself to the user
@@ -61,5 +61,5 @@ public:
 
 };
 
-// CLOSE INCLUDE GUARD to end the protected status of the code between the guard
+// CLOSE INCLUDE GUARD
 #endif
